90ff8372596f0764029b4b7ae3ab6a740eeb0ffe
Pedro Pereira <pfpereira2004@gmail.com>
2024-04-16 00:09:42 +0100

    feat: setup minimal connection

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define FIFO_NAME "task_fifo"
 
 int main() {
-  printf("Orchestrator\n");
-  return 0;
+    int fd;
+    char task[256];
+
+    mkfifo(FIFO_NAME, 0666);
+
+    fd = open(FIFO_NAME, O_RDONLY);
+
+    while (read(fd, task, sizeof(task)) > 0) {
+        printf("Received task: %s\n", task);
+    }
+
+    close(fd);
+
+    return 0;
 }
