bc279d91b35aee547b92fb469495d00cd136f7af
Pedro Pereira <109802203+pedrofp4444@users.noreply.github.com>
2024-04-16 00:40:17 +0100

    feat: small working system with suggestions (#4)

--- src/client.c
+++ src/client.c
@@ -1,22 +1,22 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define FIFO_NAME "task_fifo"
 
 int main() {
-    int fd;
-    char task[256];
+  int fd;
+  char task[256];  // It is needed to minimize the buffer size
 
-    fd = open(FIFO_NAME, O_WRONLY);
+  fd = open(FIFO_NAME, O_WRONLY);
 
-    strcpy(task, "Example of task from client");
-    write(fd, task, strlen(task) + 1);
+  strcpy(task, "Example of task from client");
+  write(fd, task, strlen(task) + 1);
 
-    close(fd);
+  close(fd);
 
-    return 0;
+  return 0;
 }
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,24 +1,42 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define FIFO_NAME "task_fifo"
+#define MAX_TASKS 100
+
+// Still not sure of the structure of the task, so i letf a suggestion
+typedef struct {
+  int id;
+  char program[256];  // Adapt size according to our needs
+  int duration;
+  pid_t pid;
+} Task;
 
 int main() {
-    int fd;
-    char task[256];
+  int fd;
+  char buffer[300];  // It is needed to minimize the buffer size
+  Task tasks[MAX_TASKS];
+  int tasks_count = 0;
 
-    mkfifo(FIFO_NAME, 0666);
+  mkfifo(FIFO_NAME, 0666);
 
-    fd = open(FIFO_NAME, O_RDONLY);
+  fd = open(FIFO_NAME, O_RDONLY);
+  if (fd == -1) {
+    perror("Error opening FIFO for reading");
+    exit(EXIT_FAILURE);
+  }
 
-    while (read(fd, task, sizeof(task)) > 0) {
-        printf("Received task: %s\n", task);
-    }
+  while (read(fd, buffer, sizeof(buffer)) > 0) {
+    // TODO: it is necessary to define among us the way to manage and resolve the tasks
+    printf("Received task: %s\n", buffer);
+  }
 
-    close(fd);
+  close(fd);
 
-    return 0;
+  return 0;
 }
