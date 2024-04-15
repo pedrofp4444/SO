1907ed980df8a1d5f476889eb3a0f18a87eb1c47
Pedro Pereira <109802203+pedrofp4444@users.noreply.github.com>
2024-04-16 00:15:53 +0100

    feat: setup minimal connection (#3)
    * feat: setup minimal connection
    
    * chore: temporary add task_fifo to make clean

--- src/client.c
+++ src/client.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#define FIFO_NAME "task_fifo"
 
 int main() {
-  printf("Client\n");
-  return 0;
+    int fd;
+    char task[256];
+
+    fd = open(FIFO_NAME, O_WRONLY);
+
+    strcpy(task, "Example of task from client");
+    write(fd, task, strlen(task) + 1);
+
+    close(fd);
+
+    return 0;
 }
