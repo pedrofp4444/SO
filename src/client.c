7cda7084062045405fcfc3194f8e1cea87cee790
Afonso Santos <113265283+Afonso-santos@users.noreply.github.com>
2024-04-22 16:07:29 +0100

    Feat: system calls (#6)
    * feat:comunication between client and server
    
    * Run format
    
    * fix: orchestrator
    
    * fix:some resquest
    
    * refactor client

--- src/client.c
+++ src/client.c
@@ -3,20 +3,100 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-#define FIFO_NAME "task_fifo"
+#define CLIENT "tmp/client_fifo"
+#define SERVER "tmp/server_fifo"
 
-int main() {
-  int fd;
-  char task[256];  // It is needed to minimize the buffer size
+#define ASK "Enter the task (format: execute <duration> <program>):\n"
 
-  fd = open(FIFO_NAME, O_WRONLY);
+typedef struct {
+  char program[256];
+  int size;
+  int duration;
+  pid_t pid;
+} Task;
 
-  strcpy(task, "Example of task from client");
-  write(fd, task, strlen(task) + 1);
+int main(int argc, char* argv[]) {
+  int fd_servidor, fd_cliente;
 
-  close(fd);
 
+  if (argc < 2) {
+    write(
+      STDOUT_FILENO,
+      "Invalid input format. Please use format: execute <duration> <program>\n",
+      71
+    );
+    exit(0);
+  }
+
+  if (fork() == 0) {
+    Task task;
+    task.duration = atoi(argv[2]);  // Convert duration argument to integer
+    task.size = argc - 3;           // Calculate the number of program arguments
+    task.pid = getpid();
+
+    // Check if memory allocation is successful
+    if (task.program == NULL) {
+      perror("Memory allocation failed");
+      exit(EXIT_FAILURE);
+    }
+    // Copy program arguments to task.program
+
+    for (int i = 3; i < argc; i++) {
+      strcat(task.program, argv[i]);
+      if (i != argc - 1) {  // Add space if not the last argument
+        strcat(task.program, " ");
+      }
+    }
+
+
+    ssize_t bytes_read = 0;
+    char fifo_name[50];
+    sprintf(fifo_name, CLIENT "_%d", task.pid);
+
+    // Create client FIFO
+
+    if (mkfifo(fifo_name, 0644) < 0) {
+      perror("Error creating FIFO");
+      exit(1);
+    }
+
+    // Open server FIFO
+
+    fd_servidor = open(SERVER, O_WRONLY);
+
+    if (fd_servidor < 0) {
+      perror("Error opening server FIFO for writing");
+      exit(1);
+    }
+
+    // Write task to server
+    write(fd_servidor, &task, sizeof(task));
+
+    // Open client FIFO for reading
+    sprintf(fifo_name, CLIENT "_%d", getpid());
+
+    if ((fd_cliente = open(fifo_name, O_RDONLY)) < 0) {
+      perror("Error opening FIFO");
+      exit(1);
+    }
+
+    // Read from client FIFO and write to stdout
+    char buffer[1024];
+    while ((bytes_read = read(fd_cliente, buffer, sizeof(buffer))) > 0) {
+      write(STDOUT_FILENO, buffer, bytes_read);
+    }
+    close(fd_cliente);
+    _exit(0);
+
+  }
+  else {
+    wait(NULL);
+  }
+
+  close(fd_servidor);
   return 0;
 }
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,42 +1,101 @@
-#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-#define FIFO_NAME "task_fifo"
-#define MAX_TASKS 100
+#define SERVER "tmp/server_fifo"
+#define CLIENT "tmp/client_fifo"
 
-// Still not sure of the structure of the task, so i letf a suggestion
 typedef struct {
-  int id;
-  char program[256];  // Adapt size according to our needs
+  char program[256];
+  int size;
   int duration;
   pid_t pid;
 } Task;
 
 int main() {
   int fd;
-  char buffer[300];  // It is needed to minimize the buffer size
-  Task tasks[MAX_TASKS];
-  int tasks_count = 0;
-
-  mkfifo(FIFO_NAME, 0666);
-
-  fd = open(FIFO_NAME, O_RDONLY);
-  if (fd == -1) {
-    perror("Error opening FIFO for reading");
-    exit(EXIT_FAILURE);
+  // Create server FIFO
+  if (mkfifo(SERVER, 0644) < 0) {
+    perror("Error creating server FIFO");
+    return 1;
   }
 
-  while (read(fd, buffer, sizeof(buffer)) > 0) {
-    // TODO: it is necessary to define among us the way to manage and resolve the tasks
-    printf("Received task: %s\n", buffer);
+  // Open server FIFO for reading CLIENT
+  if ((fd = open(SERVER, O_RDONLY)) < 0) {
+    perror("Error opening server FIFO for reading");
+    return 1;
   }
 
-  close(fd);
+  while (1) {
+    Task task;
+    int bytes_read = 0;
+    while ((bytes_read = read(fd, &task, sizeof(task))) > 0) {
+      if (fork() == 0) {
+        printf("Received task_%d: %s\n",task.pid ,task.program);
+        // Child process
 
+        char* intructions[task.size];
+        char* token = strtok(task.program, " ");
+        int i = 0;
+        while (token != NULL) {
+          intructions[i] = token;
+          token = strtok(NULL, " ");
+          i++;
+        }
+        intructions[i++] = NULL;
+
+        // Open client FIFO for writing response
+        char fifo_name[50];
+        sprintf(fifo_name, CLIENT "_%d", task.pid);
+        int fd_client = open(fifo_name, O_WRONLY);
+        if (fd_client < 0) {
+          perror("Error opening client FIFO for writing response");
+          return 1;
+        }
+
+        // Redirect stdout to a pipe
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+          perror("pipe");
+          return 1;
+        }
+
+        if (fork() == 0) {
+          // Child process to execute the task
+          close(pipefd[0]);
+          dup2(
+            pipefd[1], STDOUT_FILENO
+          );  // Redirect stdout to the write end of the pipe
+          close(pipefd[1]);
+
+          // Execute task
+          execvp(intructions[0], intructions);
+          perror("execvp");
+          exit(1);
+        }
+        else {
+          // Parent process
+          close(pipefd[1]);
+          char buffer[1024];
+          ssize_t nbytes = 0;
+          while ((nbytes = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+            // Write the output of the task back to the client
+            write(fd_client, buffer, nbytes);
+          }
+          close(pipefd[0]);
+
+          // Close client FIFO and exit
+          close(fd_client);
+          write(STDOUT_FILENO, "Task completed\n", 15);
+          _exit(0);
+        }
+      }
+    }
+  }
   return 0;
 }
